4f7ddb01e71298adce19e9c4ac7171ade74a4054
Jugal Mistry <jugalmistry99@gmail.com>
2021-04-24 22:37:34 +0530

    Add Damage System

--- Assets/Scripts/Health/Damageable.cs
+++ Assets/Scripts/Health/Damageable.cs
@@ -2,8 +2,25 @@ using UnityEngine;
 
 namespace Health {
 
-    public class Damageable {
+    public abstract class Damageable {
 
-        
+        protected float maxHP;
+        protected float maxEnergy;
+        protected float maxStamina;
+        protected float defense; // varies from 0 to 100
+        protected float attack; // varies from 0 to 100
+
+
+        protected float currentHP;
+        protected float currentEnergy;
+        protected float currentStamina;
+
+        public void ReceiveDamage(float damage) {
+            currentHP = currentHP - 100 * damage / (100 + defense);
+        }
+
+        public void DealDamage(Damageable opponent) {
+            opponent.ReceiveDamage(this.attack);
+        }
     }
 }
